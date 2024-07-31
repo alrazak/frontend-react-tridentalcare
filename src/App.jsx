b77f90149993bbe84ad1f0ef757404366288145d
alrazak <abdrzak1129@gmail.com>
2024-07-31 21:09:31 +0700

    init react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+      <div className="p-5 mb-4 bg-light rounded-3 shadow-sm">
+          <div className="container-fluid py-5">
+              <h1 className="display-5 fw-bold">TRI DENTAL CARE</h1>
+              <p className="col-md-12 fs-4">Melayani perapian gigi menggunakan metode behel damon dll.</p>
+              <hr />
+          </div>
+      </div>
+  )
+}
