1a388ccf29a2ac4abbcf89c5f5af0852f844255f
alrazak <abdrzak1129@gmail.com>
2024-08-01 22:53:05 +0700

    add: footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="bg-light py-3 mt-5">
+            <div className="container text-center">
+                <p className="mb-0">© 2024 TRI DENTAL CARE. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
