c641208b50db08ad8a2af25c62aa628e51000fe8
alrazak <abdrzak1129@gmail.com>
2024-08-01 22:53:49 +0700

    add: header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="bg-light py-3 shadow-sm mb-5">
+            <div className="container">
+                <h1 className="display-4 fw-bold">Tri Dental Care</h1>
+                <p className="lead">Melayani perapian gigi menggunakan behel damon</p>
+                <hr />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
