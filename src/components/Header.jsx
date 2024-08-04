a9617ff6110b05bfc56ab574e1267ed146c044b9
alrazak <abdrzak1129@gmail.com>
2024-08-04 16:42:02 +0700

    put: header navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
-    return (
-        <header className="bg-light py-3 shadow-sm mb-5">
-            <div className="container">
-                <h1 className="display-4 fw-bold">Tri Dental Care</h1>
-                <p className="lead">Melayani perapian gigi menggunakan behel damon</p>
-                <hr />
-            </div>
-        </header>
-    )
+  return (
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Navbar.Brand href="#home">Tri Dental Care</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default Header;
