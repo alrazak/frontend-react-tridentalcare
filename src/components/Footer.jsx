af8398de02c5079cef58c63c170c62fff8be0573
alrazak <abdrzak1129@gmail.com>
2024-08-05 16:35:10 +0700

    put: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer className="bg-light py-3 mt-5">
+        <footer className="bg-primary text-white py-1 mt-auto">
             <div className="container text-center">
-                <p className="mb-0">© 2024 TRI DENTAL CARE. All rights reserved.</p>
+                <p className="mb-0">© 2024 Tri Dental Care. All rights reserved.</p>
             </div>
         </footer>
     );
