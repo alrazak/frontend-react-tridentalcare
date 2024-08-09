ab39cbe50ee8b6e8618a171ff1c07d788ad80119
alrazak <abdrzak1129@gmail.com>
2024-08-09 20:31:52 +0700

    put: navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function CustomNavbar() {
+  return (
+    <div>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home" className="text-white">Tri Dental Care</Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link href="#about" className="text-white">About</Nav.Link>
+            <Nav.Link href="#contact" className="text-white">Contact</Nav.Link>
+            <Nav.Link href="#register" className="text-white">Register</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default CustomNavbar;
