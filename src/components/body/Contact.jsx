ad08b7f4773b297317e7511289de27f7cdaecf31
alrazak <abdrzak1129@gmail.com>
2024-08-12 20:12:13 +0700

    put: styling contact

--- src/components/body/Contact.jsx
+++ src/components/body/Contact.jsx
@@ -2,23 +2,22 @@ import React from "react";
 
 function Contact() {
     return (
-        <div id="contact" className="row d-flex justify-content-center mt-5">
-            <h3 className="card-title fw-bold text-center mb-3">Jadwal Operasional Klinik</h3>
-            <div className="col-md-6">
-                <div className="card mb-4 bg-primary text-white shadow-sm">
-                    <div className="card-body text-center">
-                        
-                        <p className="card-text">Senin - Jumat : 10.00 - 21.00 Wib</p>
-                        <p className="card-text">Sabtu : 10.00 - 18.00 Wib</p>
-                        <p className="card-text">Minggu/hari libur : Tutup</p>
+        <div id="contact" className="bg-black text-center py-5">
+            <h3 className="text-white fw-bold mb-4">Jadwal Operasional Klinik</h3>
+            <div className="container d-flex justify-content-center">
+                <div className="card mb-5 bg-primary text-white" style={{ maxWidth: '600px', width: '100%' }}>
+                    <div className="card-body bg-white text-black text-center">
+                        <p className="card-text">Senin - Jumat: 10.00 - 21.00 WIB</p>
+                        <p className="card-text">Sabtu: 10.00 - 18.00 WIB</p>
+                        <p className="card-text">Minggu/hari libur: Tutup</p>
                     </div>
-                    <div className="bg-black text-white text-center shadow-sm">
-                        <p className="card-text">Instagram : @tri.dentalcare | Email : info.tridentalcare@gmail.com</p>
+                    <div className="card-footer bg-primary text-white text-center">
+                        <p className="card-text">Instagram: @tri.dentalcare | Email: info.tridentalcare@gmail.com</p>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Contact;
