011e8a78fae4130b21ab8c7c4326a766d1220539
alrazak <abdrzak1129@gmail.com>
2024-08-31 14:53:34 +0700

    put: description header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Header() {
     return (
@@ -8,8 +7,8 @@ function Header() {
                 <div className="container-fluid py-5">
                     <h1 className="display-5 fw-bold">Selamat Datang di Website Tri Dental Care Sidoarjo</h1>
                     <p className="col-md-12 fs-4">
-                        Melayani perapian gigi menggunakan behel sehat dan ditangani dengan dokter spesialis. Dengan fasilitas memadai dan peralatan selalu steril, kami berkompeten melayani perawatan behel gigi 
-                        <Link to="/Card" className="text-black"> selengkapnya..</Link>
+                    Kami menyediakan layanan perapian gigi dengan behel yang sehat, ditangani oleh dokter spesialis. Dengan fasilitas yang memadai dan peralatan yang selalu steril, kami berkomitmen untuk memberikan perawatan behel gigi terbaik bagi Anda
+                        <a href="/Card" className="text-black"> selengkapnya..</a>
                     </p>
                 </div>
             </div>
