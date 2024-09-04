204d2cc15675dadca1b5d424bd5ec8d5e01de9d0
alrazak <abdrzak1129@gmail.com>
2024-09-04 18:13:42 +0700

    put: navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function CustomNavbar() {
   return (
     <>
-      <Navbar className="navbar-custom" expand="lg" fixed="top">
+      <Navbar className="navbar-custom" expand="lg" fixed="top" collapseOnSelect>
         <Navbar.Brand href="#home">Tri Dental Care</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto nav-items">
-            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#about">Tentang Kami</Nav.Link>
             <Nav.Link href="#testimonial">Testimoni</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#contact">Hubungi Kami</Nav.Link>
             <Nav.Link href="#register">Register</Nav.Link>
           </Nav>
         </Navbar.Collapse>
@@ -37,6 +37,11 @@ function CustomNavbar() {
         .navbar-custom .nav-link {
           font-size: 1em;
           color: #fff !important;
+          margin-right: 15px;
+        }
+
+        .navbar-custom .nav-link:last-child {
+          margin-right: 0; /* Menghilangkan margin pada item terakhir */
         }
 
         .navbar-custom .nav-link:hover {
