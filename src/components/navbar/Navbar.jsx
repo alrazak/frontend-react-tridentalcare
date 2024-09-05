f32dec9ab3d8e97835c627ff8d834b6d8eff0492
alrazak <abdrzak1129@gmail.com>
2024-09-05 17:13:32 +0700

    add: menu register

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function CustomNavbar() {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
     <>
       <Navbar className="navbar-custom" expand="lg" fixed="top" collapseOnSelect>
@@ -12,7 +14,20 @@ function CustomNavbar() {
             <Nav.Link href="#about">Tentang Kami</Nav.Link>
             <Nav.Link href="#testimonial">Testimoni</Nav.Link>
             <Nav.Link href="#contact">Hubungi Kami</Nav.Link>
-            <Nav.Link href="#register">Register</Nav.Link>
+            {/* Menu Register dengan dropdown */}
+            <div
+              className="nav-link-dropdown"
+              onMouseEnter={() => setIsHovering(true)}
+              onMouseLeave={() => setIsHovering(false)}
+            >
+              <Nav.Link href="#register">Register</Nav.Link>
+              {isHovering && (
+                <div className="dropdown-menu">
+                  <Nav.Link href="#buat-akun">Buat Akun</Nav.Link>
+                  <Nav.Link href="#login">Login</Nav.Link>
+                </div>
+              )}
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
@@ -40,12 +55,34 @@ function CustomNavbar() {
           margin-right: 15px;
         }
 
-        .navbar-custom .nav-link:last-child {
-          margin-right: 0; /* Menghilangkan margin pada item terakhir */
-        }
-
         .navbar-custom .nav-link:hover {
           color: #ddd !important;
+        }
+
+        .nav-link-dropdown {
+          position: relative;
+        }
+
+        .dropdown-menu {
+          position: absolute;
+          top: 100%;
+          left: 0;
+          background-color: #007bff;
+          padding: 10px;
+          display: flex;
+          flex-direction: column;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+
+        .dropdown-menu .nav-link {
+          margin-right: 0;
+          padding: 5px 10px;
+          font-size: 0.9em;
+          color: #fff;
+        }
+
+        .dropdown-menu .nav-link:hover {
+          color: #ddd;
         }
       `}</style>
     </>
