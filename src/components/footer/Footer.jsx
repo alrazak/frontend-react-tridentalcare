3b6f3663dc709f19680de6581f189eada24ea2bc
alrazak <abdrzak1129@gmail.com>
2024-09-16 16:10:46 +0700

    put: footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
                 <div className="row">
                     {/* Logo dan Deskripsi Singkat */}
                     <div className="col-md-4 col-lg-4 col-xl-3 mx-auto mt-3">
-                        <h5 className="mb-4 font-weight-bold text-decoration-underline">Tri Dental Care</h5>
+                        <h5 className="mb-4 font-weight-bold">Tri Dental Care</h5>
                         <p>
                             Klinik perawatan gigi terpercaya Anda. Memberikan pelayanan dan perawatan terbaik untuk kesehatan gigi Anda.
                         </p>
@@ -15,7 +15,7 @@ function Footer() {
 
                     {/* Tautan Menu */}
                     <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h5 className="mb-4 font-weight-bold text-decoration-underline">Tautan Cepat</h5>
+                        <h5 className="mb-4 font-weight-bold">Tautan Cepat</h5>
                         <p><a href="#" className="link-page text-white text-decoration-none">Beranda</a></p>
                         <p><a href="#" className="link-page text-white text-decoration-none">Tentang Kami</a></p>
                         <p><a href="#" className="link-page text-white text-decoration-none">Layanan</a></p>
@@ -24,7 +24,7 @@ function Footer() {
 
                     {/* Tautan Sosial Media */}
                     <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h5 className="mb-4 font-weight-bold text-decoration-underline">Ikuti Kami</h5>
+                        <h5 className="mb-4 font-weight-bold">Ikuti Kami</h5>
                         <a href="https://www.facebook.com" target="_blank" className="social-link d-block mb-3">
                             <i className="fab fa-facebook fa-2x"></i> Facebook
                         </a>
@@ -41,7 +41,7 @@ function Footer() {
 
                     {/* Kontak dan Informasi */}
                     <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <h5 className="mb-4 font-weight-bold text-decoration-underline">Info Kontak</h5>
+                        <h5 className="mb-4 font-weight-bold">Info Kontak</h5>
                         <p><i className="fas fa-home mr-3"></i> Jl. Mojopahit No.96, Setelah RSUD Sidoarjo</p>
                         <p><i className="fas fa-envelope mr-3"></i> info@tridentalcare.com</p>
                         <p><i className="fas fa-phone mr-3"></i> + 123 456 7890</p>
